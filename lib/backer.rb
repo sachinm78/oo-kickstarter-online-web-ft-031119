94d268d171acbb6c99a893e36d036c45f6114c41
Sachin Mahajan <sachinm78@gmail.com>
2019-03-25 22:01:53 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,12 @@
 class Backer
   attr_reader :name, :backed_projects
+  attr_writer: :backers
   
   def initialize(name)
     @name = name
     @backed_projects = []
   end
   
+  def add_backer(backer)
+    @backers 
 end
